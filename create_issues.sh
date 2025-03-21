2bd69094728b1e412040d4cccf1f3274240a1f66
Mark McEver <mark@fastmail.net>
2025-03-21 10:20:51 -0500

    Ensured created issues link to feature files rather than import_files

--- create_issues.sh
+++ create_issues.sh
@@ -21,7 +21,7 @@ branch='v14.7.0'
 # - We use awk to print the file because it will append a trailing newline if it is missing
 # - We use 'tr -d "\r"' to remove carriage returns in case the file has been edited on Windows
 awk 1 features.csv | tr -d "\r" | while read line; do
-    file=$(find . | grep "$line")
+    file=$(find 'Feature Tests' | grep "$line")
     if [ -z "$file" ]; then
       echo "File not found matching '$line'.  Stopping..."
       exit
