5b9027eb403d71d5b9143426ff73f275554e9c55
Mark McEver <mark@fastmail.net>
2025-08-13 21:26:53 -0500

    Parted duration from test output

--- import_video.sh
+++ import_video.sh
@@ -24,3 +24,13 @@ $CURL -H "Accept: application/json" \
       -F "filename=$FILENAME" \
       -F "file=@\"$VIDEO_FILE\"" \
       $REDCAP_API_URL
+
+ID=`echo $VIDEO_FILE | rev | cut -d/ -f 1 | rev | cut -d' ' -f 1`
+PASSING_DURATION=`grep "file=\"redcap_rsvc.*$ID" test  -r --before-context=1 | grep 'tests="1" failures="0"'| cut -d\" -f 4`
+
+if [ -z "$PASSING_DURATION" ]; then
+  echo The feature did not pass.  Skipping field updates.
+  exit
+fi
+
+echo duration: $DURATION
